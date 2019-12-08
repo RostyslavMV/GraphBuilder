482b7ea20cf9ed63883655653d150ff3e621ff62
Rostyslav M <mrostyk55@gmail.com>
2019-12-08 20:13:20 +0200

    Add project files.

---
+++ GraphBuilder/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Calc;
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace GraphBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            Plot();
+        }
+
+
+        private void Plot()
+        {
+            var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+            Evaluator evaluator = new Evaluator();
+            //double result = evaluator.Eval("Math.sin(0.5)");
+            double result1 = evaluator.EvalFunc("Math.sin(x)", 0.5);
+            try
+            {
+                var y = x.Select(v => evaluator.EvalFunc("Math.sin(x)*Math.cos(x)*x+x", v)).ToArray();
+                linegraph.Plot(x, y);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
