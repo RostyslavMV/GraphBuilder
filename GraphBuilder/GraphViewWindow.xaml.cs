15aaa5d32012cd3cf596e2751aec52490ce04dd9
Rostyslav M <mrostyk55@gmail.com>
2019-12-08 20:48:32 +0200

    MainWindow UI

---
+++ GraphBuilder/GraphViewWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Calc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace GraphBuilder
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    /// 
+    public partial class GraphViewWindow : Window
+    {
+        #region Private Propetries
+        private string Function { get; set;
+             private string X1 { get; set; }
+        private string X2 { get; set; }
+
+        #endregion
+        public GraphViewWindow()
+        {
+
+            InitializeComponent();
+            Plot();
+        }
+
+        Dictionary<string,string> convertExpressions = new Dictionary<string, string>();
+
+        private void Plot()
+        {
+            var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+            Evaluator evaluator = new Evaluator();
+            //double result = evaluator.Eval("Math.sin(0.5)");
+            double result1 = evaluator.EvalFunc("Math.sin(x)", 0.5);
+            try
+            {
+                var y = x.Select(v => evaluator.EvalFunc("Math.sin(x)*Math.cos(x)*x+x", v)).ToArray();
+                linegraph.Plot(x, y);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        public void getParameters(string Function, string X1, string X2)
+        {
+            this.Function = Function;
+            this.X1 = X1;
+            this.X2 = X2;
+        }
+    }
+}
--- GraphBuilder/MainWindow.xaml.cs
+++ GraphBuilder/MainWindow.xaml.cs
@@ -12,28 +12,13 @@ namespace GraphBuilder
     {
         public MainWindow()
         {
-
             InitializeComponent();
-            Plot();
         }
 
-
-        private void Plot()
+        private void ViewGraphButton_Click(object sender, RoutedEventArgs e)
         {
-            var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
-            Evaluator evaluator = new Evaluator();
-            //double result = evaluator.Eval("Math.sin(0.5)");
-            double result1 = evaluator.EvalFunc("Math.sin(x)", 0.5);
-            try
-            {
-                var y = x.Select(v => evaluator.EvalFunc("Math.sin(x)*Math.cos(x)*x+x", v)).ToArray();
-                linegraph.Plot(x, y);
-            }
-            catch (Exception ex)
-            {
-
-            }
-
+            var newWindow = new GraphViewWindow();
+            newWindow.Show();
         }
     }
 }
